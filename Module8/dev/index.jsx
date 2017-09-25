b46952c8dbfaa5ae881b0a335e931cff612b2322
Dave Fancher <dave@davefancher.com>
2017-09-25 08:40:12 -0400

    Added DevTools connection to sample project

--- Module8/dev/index.jsx
+++ Module8/dev/index.jsx
@@ -7,7 +7,10 @@ import AppContainer from "./containers/appContainer.jsx";
 
 require("./site.scss");
 
-const store = createStore(AuthReducer);
+const store = createStore(
+    AuthReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
     <Provider store={store}>
